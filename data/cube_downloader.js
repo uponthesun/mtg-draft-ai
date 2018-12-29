608a9ba94899298c3d55e43e1a58929f0fa07fd3
Matthew McKay <mattmckayj@gmail.com>
2018-12-28 21:42:07 -0800

    Change download script to retrieve page data vis AJAX calls
    Now the script makes POST requests directly to get the data for each page of the cube, without relying on the current page.
    
    Several other minor fixes were also done to clean up the code, and an example file of cube tag data has been added.

--- data/cube_downloader.js
+++ data/cube_downloader.js
@@ -9,8 +9,19 @@
 
 'use strict';
 
+// This is the last element of the search filter section of the UI, so the download
+// button is added right after it so it is in a convenient place.
+var SEARCH_FILTER_ELEMENT_ID = 'accordion_0'
+var PAGE_LIST_ELEMENT_ID = 't-data-grid-pager'
+
+// Ids for the generated downloaded button and its container.
 var BUTTON_ID = 'download-card-tags';
 var BUTTON_CONTAINER_ID = 'download-card-tags-button-container';
+
+// The regular expression for pulling the cube id out of the URL. This finds the first number in
+// in the URL preceded by a '/' and matches all of it, enforcing that we either see a non-numeral
+// or the end of the string after.
+var CUBE_ID_REGEX = /.*\/([0-9]*)[^0-9]*.*$/;
 
 /**
  * The main function of this script, which gets run on execution.
@@ -21,46 +32,65 @@ var BUTTON_CONTAINER_ID = 'download-card-tags-button-container';
     buttonNode.innerHTML = '<button id="' + BUTTON_ID + '" type="button">Download Card Tags</button>';
     buttonNode.setAttribute ('id', BUTTON_CONTAINER_ID);
 
-    document.getElementById('accordion_0').appendChild(buttonNode);
+    document.getElementById(SEARCH_FILTER_ELEMENT_ID).appendChild(buttonNode);
 
     // Calls out to downloadCardInfo() when the added button is clicked.
     document.getElementById(BUTTON_ID).addEventListener( 'click', downloadCardInfo, false);
 })();
 
 /**
- * A starter method for downloadCardInfoRecursive(). Note that this assumes that you are on
- * the first page of the cube.
+ * A starter method for downloadCardPage(). This pulls out the number of pages from the page selector
+ * element and uses regular expressions to get the cube id number from the URL. It then calls a method
+ * that recursively downloads every page's data.
  */
 function downloadCardInfo() {
-    downloadCardInfoRecursive('', 0);
-}
-/**
- * The primary method for downloading the card data from cube tutor, which gets executed upon
- * the button from the main method being clicked. Walks the HTML to read each card's row and
- * the properties in each column, loading each subsequent page and retrieving its contents as well.
- *
- * Since the individual pages are not separate page loads, we can click through them in Javascript
- * to load the next page's data, but we have to wait a second before processing the DOM elements
- * to give them time to update.
- */
-function downloadCardInfoRecursive(content, page) {
-    content += downloadCardPageInfo();
-    page +=1;
-
-    var tablePages = document.getElementsByClassName('t-data-grid-pager')[0];
+    var tablePages = document.getElementsByClassName(PAGE_LIST_ELEMENT_ID)[0];
     var numPages = tablePages.childNodes.length;
 
+    var url = window.location.href
+    var cubeId = CUBE_ID_REGEX.exec(url)[1]; // The first matched group is at index 1.
+
+    downloadCardPage('', 1, numPages, cubeId);
+}
+
+/**
+ * Downloads a single page of card data from Cube Tutor. Using the arguments, constructs a URL to
+ * request the given page from Cube Tutor then walks the HTML to read each card's row and
+ * the properties in each column. This is done in a callback from the request to get the page data.
+ *
+ * After the data is retrieved, it gets added to the content and the next page is downloaded recursively,
+ * until we reach numPages. At that point the current content is saved to a file.
+ *
+ * @param {string} content The generated output file content downloaded so far.
+ * @param {number} page The page to download when running this function.
+ * @param {number} numPages The total number of pages to download, so we know when to stop.
+ * @param {string} cubeId The id of the current cube, for generating the URL to POST to.
+ */
+function downloadCardPage(content, page, numPages, cubeId) {
     // If we're on the last page, save the file with all the content that we have so far.
-    if(page == numPages) {
+    if(page > numPages) {
         saveFile(content);
-    // Otherwise, load the next page and wait a second before downloading it.
+        // Otherwise, load the next page and wait a second before downloading it.
     } else {
-        var pageNodes = tablePages.childNodes[page].click();
-        // Wait a second before calling back to get the next page's data.
-        setTimeout(function () {
-            downloadCardInfoRecursive(content, page);
-        }, 1000);
+        // Download the specified page by performing a POST against a generated URL for the cube.
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            // This function gets called multiple times, we only want to process the result in this case.
+            if (this.readyState == 4 && this.status == 200) {
+                var doc = generateDocumentFromString(this.responseText);
+                content += downloadCardPageInfo(doc);
+                downloadCardPage(content, page + 1, numPages, cubeId);
+            }
+        };
+        xhttp.open('POST', 'http://www.cubetutor.com/editcube.grid.pager/' + page + '/grid?t:ac=' + cubeId, true);
+        xhttp.send();
     }
+}
+
+function generateDocumentFromString(html) {
+    var element = document.createElement('div');
+    element.innerHTML = html;
+    return element;
 }
 
 /**
@@ -69,8 +99,8 @@ function downloadCardInfoRecursive(content, page) {
  *
  * @return {string} The content of all the cards for the current page.
  */
-function downloadCardPageInfo() {
-    var tableBody = document.getElementById('editCubeGrid').lastChild;
+function downloadCardPageInfo(doc) {
+    var tableBody = doc.getElementsByClassName('t-data-grid')[1].lastChild;
     var tableRows = tableBody.childNodes;
 
     var content = '';
@@ -97,13 +127,13 @@ function downloadCardPageInfo() {
 /**
  * Converts a DOM node containing a card's mana cost into an array of strings.
  *
- * @param {Element} symbolColumn The DOM element containing the card's mana cost.
+ * @param {Element} symbolsElement The DOM element containing the card's mana cost.
  * @return {string[]} The array of mana symbols represented as strings.
  */
-function parseManaCost(symbolColumn) {
-    var symbolNodes = Array.from(symbolColumn.childNodes);
+function parseManaCost(symbolsElement) {
+    var symbolNodes = Array.from(symbolsElement.childNodes);
     var manaCost = symbolNodes.map(convertManaSymbol);
-    return arrayToString(manaCost);
+    return JSON.stringify(manaCost);
 }
 
 /**
@@ -114,7 +144,7 @@ function parseManaCost(symbolColumn) {
  */
 function convertManaSymbol(symbolNode) {
     var symbol = symbolNode.className;
-    symbol = symbol.replace("symbol", "");
+    symbol = symbol.replace('symbol', '');
     return symbol;
 }
 
@@ -122,11 +152,11 @@ function convertManaSymbol(symbolNode) {
  * Converts a DOM node containing a color identity selector into the string
  * representing the selected color identity.
  *
- * @param {Element} colorColumn The DOM element containing the card's color identity.
+ * @param {Element} colorElement The DOM element containing the card's color identity.
  * @return {string} The string representing the color identity of the card.
  */
-function parseColorIdentity(colorColumn) {
-    var identity = getSelected(colorColumn).text;
+function parseColorIdentity(colorElement) {
+    var identity = getSelected(colorElement).text;
     // Change colorless to 'C' for consistency
     identity = identity.replace('Colourless', 'C');
     // Cut to the first word to remove unnecessary information in parentheses.
@@ -137,23 +167,23 @@ function parseColorIdentity(colorColumn) {
 /**
  * Converts a DOM node containing a type selector into an array of the types of the card.
  *
- * @param {Element} typeColumn The DOM element containing the card's type.
+ * @param {Element} typeElement The DOM element containing the card's type.
  * @return {string[]} An array of strings containing the types of the card.
  */
-function parseTypes(typeColumn) {
-    var types = getSelected(typeColumn).value.split('_');
+function parseTypes(typeElement) {
+    var types = getSelected(typeElement).value.split('_');
     types = types.map(t => t.toLowerCase());
-    return arrayToString(types);
+    return JSON.stringify(types);
 }
 
 /**
  * Converts a DOM node containing tag information into an array of the tags of the card.
  *
- * @param {Element} tagsColumn The DOM element containing the card's tags.
+ * @param {Element} tagsElement The DOM element containing the card's tags.
  * @return {string[]} An array of strings containing the tags of the card.
  */
-function parseTags(tagsColumn) {
-    var tags = tagsColumn.firstChild.getAttribute('data-tags');
+function parseTags(tagsElement) {
+    var tags = tagsElement.firstChild.getAttribute('data-tags');
     return tags == null ? '[]' : tags;
 
 }
@@ -166,24 +196,6 @@ function parseTags(tagsColumn) {
  */
 function getSelected(selector) {
     return selector.options[selector.selectedIndex];
-}
-
-/**
- * Converts an array to a parseable printed string.
- *
- * @param {Array} array The array to stringify.
- * @return {string} The stringified array
- */
-function arrayToString(array) {
-    if(array.length == 0) {
-        return '[]'
-    }
-    var result = '[' + quoteString(array[0]);
-    for(var i = 1; i < array.length; i++) {
-        result += ', ' + quoteString(array[i]);
-    }
-    result += ']';
-    return result;
 }
 
 /**
