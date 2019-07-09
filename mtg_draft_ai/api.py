ebe71135ed4f0a98e338f746a086f7cc3ae3bac5
Winter Dong <winter@code.org>
2019-07-09 00:23:44 -0700

    undo repr change

--- mtg_draft_ai/api.py
+++ mtg_draft_ai/api.py
@@ -28,12 +28,11 @@ class Card:
         self.tags = tags
         self.power_tier = power_tier
 
-    def full_repr(self):
-        """Returns full string representation of this card."""
-        return '{}: {}'.format(self.__class__.__name__, repr(self.__dict__))
+    def __str__(self):
+        return 'C: {}'.format(self.name)
 
     def __repr__(self):
-        return 'C: {}'.format(self.name)
+        return '{}: {}'.format(self.__class__.__name__, repr(self.__dict__))
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
