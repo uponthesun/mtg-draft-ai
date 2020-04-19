999d47662161f31a31ab34ac48838c350f41eda0
Winter Dong <winter@code.org>
2020-04-19 15:50:27 -0700

    Add initial tag and fixer data files for cube 6949

--- get_fixer_data.py
+++ get_fixer_data.py
@@ -1,6 +1,9 @@
+import sys
 import requests
 import toml
 
+
+cube_list_file = sys.argv[1]
 
 def _get_color_id(name):
     r = requests.get('https://api.scryfall.com/cards/named', params={'exact': name})
@@ -8,10 +11,8 @@ def _get_color_id(name):
     return ''.join(card_json['color_identity'])
 
 
-cards = toml.load('cube_81183_tag_data.toml')
-#cards = toml.load('cube_6949_tag_data.toml')
+cards = toml.load(cube_list_file)
 fixers = [k for k, v in cards.items() if 'Fixer' in v['tags']]
 fixers_with_color_ids = {name: _get_color_id(name) for name in fixers}
 
-with open('cube_81183_fixer_data.toml', 'w') as f:
-    toml.dump(fixers_with_color_ids, f)
+print(toml.dumps(fixers_with_color_ids))
