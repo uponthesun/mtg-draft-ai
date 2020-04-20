69a8e98784e95ee07a65d2130a37d93870f97844
Winter <winter@code.org>
2020-04-19 18:25:23 -0700

    Use cached image urls for all_picks (#62)

--- website/draft_site/drafts/views/all_picks.py
+++ website/draft_site/drafts/views/all_picks.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 
+from .constants import CUBES_BY_ID
 from .. import models
-
-from mtg_draft_ai.api import Drafter
-from mtg_draft_ai.draftlog import drafters_to_html
 
 
 # /draft/<int:draft_id>/seat/<int:seat>/all-picks
 def all_picks(request, draft_id, seat):
     draft = get_object_or_404(models.Draft, pk=draft_id)
     drafter = draft.drafter_set.get(seat=seat)
+    cube_data = CUBES_BY_ID[draft.cube_id]
 
-    output = _convert_drafter(drafter)
+    picks, pack_history = _convert_drafter(drafter)
+    picks_urls = [cube_data.get_image_url(name) for name in picks]
+    pack_leftovers_urls = [[cube_data.get_image_url(name) for name in pack] for pack in pack_history]
 
     context = {
         'draft': draft,
         'drafter': drafter,
         'seat_range': range(0, draft.num_drafters),  # Used by header
-        'output': output,
+        'picks_with_packs': zip(picks_urls, pack_leftovers_urls),
     }
     return render(request, 'drafts/all_picks.html', context)
 
@@ -33,9 +34,9 @@ def _convert_drafter(db_drafter):
         picks = models.Card.objects.filter(draft=draft, picked_by=d)
         sorted_picks = sorted(picks, key=lambda c: (c.phase, c.picked_at))
         picks_table.append([c.name for c in sorted_picks])
-    cards_owned = picks_table[db_drafter.seat]
 
-    pack_history = []
+    picks = []
+    pack_leftovers = []
     for phase in range(0, draft.num_phases):
         direction = -1 if phase % 2 == 0 else 1
 
@@ -48,10 +49,7 @@ def _convert_drafter(db_drafter):
                 pack_contents.append(picks_table[r][c])
                 r = (r + direction) % draft.num_drafters
                 c += 1
-            pack_history.append(pack_contents)
+            picks.append(pack_contents.pop(0))
+            pack_leftovers.append(pack_contents)
 
-    result = Drafter(None, None)
-    result.cards_owned = cards_owned
-    result.pack_history = pack_history
-
-    return drafters_to_html([result], headers=False)
+    return picks, pack_leftovers
