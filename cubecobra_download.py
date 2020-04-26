e01b4fcb801e1dfc7504b508f35c205c0817ed4b
Winter Dong <winter@code.org>
2020-04-26 16:24:32 -0700

    Only fetch mana cost from scryfall if we don't already have it

--- cubecobra_download.py
+++ cubecobra_download.py
@@ -5,9 +5,13 @@ import toml
 import requests
 
 cube_id = sys.argv[1]
-output_file_name = sys.argv[2]
+data_file_name = sys.argv[2]
 
-response = requests.get('https://cubecobra.com/cube/download/csv/{}?primary=Color%20Category&secondary=Types-Multicolor&tertiary=CMC2'.format(cube_id))
+with open(data_file_name, 'r') as f:
+    CURRENT_DATA = toml.load(f)
+
+url = 'https://cubecobra.com/cube/download/csv/{}?primary=Color%20Category&secondary=Types-Multicolor&tertiary=CMC2'
+response = requests.get(url.format(cube_id))
 lines = [row for row in response.content.decode('utf-8').split('\r\n') if len(row) > 0]
 reader = csv.reader(lines)
 csv_rows = [row for row in reader]
@@ -21,11 +25,16 @@ def type_line_to_types(type_line):
     types = type_line.split('-')[0].strip().split(' ')
     return [t.lower() for t in types if t != 'Legendary']
 
+
 def get_mana_cost(name):
+    if 'mana_cost' in CURRENT_DATA[name]:
+        return CURRENT_DATA[name]['mana_cost']
+
     mana_cost_scryfall = get_mana_cost_scryfall(name)
     result = list(mana_cost_scryfall.replace('{', '').replace('}', ''))
-    print('{}: {}'.format(name, result))
+    print('Mana cost from scryfall for {}: {}'.format(name, result))
     return result
+
 
 def get_mana_cost_scryfall(name):
     try:
@@ -41,6 +50,7 @@ def get_mana_cost_scryfall(name):
     finally:
         time.sleep(0.2)
 
+
 def convert_cubecobra_card(name, card):
     return {
         'mana_cost': get_mana_cost(name),
@@ -49,7 +59,8 @@ def convert_cubecobra_card(name, card):
         'tags': [t.strip() for t in card['Tags'].split(',')]
     }
 
+
 converted_cards_by_name = {name: convert_cubecobra_card(name, card) for name, card in cards_by_name.items()}
 
-with open(output_file_name, 'w') as f:
+with open(data_file_name, 'w') as f:
     toml.dump(converted_cards_by_name, f)
