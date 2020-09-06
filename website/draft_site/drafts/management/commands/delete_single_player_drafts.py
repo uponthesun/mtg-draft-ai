d85f99b3a3e7f5d6c4cf3a90f560be36f076d254
Winter Dong <winter@code.org>
2020-09-06 16:31:24 -0700

    Fix deletion script to delete in batches

--- website/draft_site/drafts/management/commands/delete_single_player_drafts.py
+++ website/draft_site/drafts/management/commands/delete_single_player_drafts.py
@@ -2,6 +2,9 @@ from django.core.management.base import BaseCommand
 from drafts.models import Draft, Drafter
 
 
+DELETE_BATCH_SIZE = 100
+
+
 class Command(BaseCommand):
     help = 'Deletes drafts that only have one human player'
 
@@ -24,10 +27,13 @@ class Command(BaseCommand):
         single_player_drafts = [draft for draft in all_drafts if len(human_drafters_by_draft_id[draft.id]) <= 1]
 
         drafts_to_delete = Draft.objects.filter(id__in=[draft.id for draft in single_player_drafts])
+        draft_ids_to_delete = [draft.id for draft in drafts_to_delete]
 
-        print('About to delete {} drafts: {}'.format(len(drafts_to_delete), [draft.id for draft in drafts_to_delete]))
-        if options['dry_run']:
-            print('--dry-run specified; nothing deleted')
-        else:
-            deletions = drafts_to_delete.delete()
-            print('Deletions: {}'.format(deletions))
+        print('About to delete {} drafts: {}'.format(len(drafts_to_delete), draft_ids_to_delete))
+        for i in range(0, len(draft_ids_to_delete), DELETE_BATCH_SIZE):
+            batch = draft_ids_to_delete[i:i + DELETE_BATCH_SIZE]
+            if options['dry_run']:
+                print('--dry-run specified; nothing deleted, would have deleted: {}'.format(batch))
+            else:
+                deletions = Draft.objects.filter(id__in=batch).delete()
+                print('Deletions: {}'.format(deletions))
