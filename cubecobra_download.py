fabc81f7ae1f60b67bf721c755f2f30962948093
Winter Dong <uponthesun@gmail.com>
2021-06-09 17:30:35 -0700

    List update

--- cubecobra_download.py
+++ cubecobra_download.py
@@ -57,7 +57,7 @@ def convert_cubecobra_card(name, card):
         'mana_cost': get_mana_cost(name),
         'color_identity': card['Color'],
         'types': type_line_to_types(card['Type']),
-        'tags': [t.strip() for t in card['Tags'].split(',')],
+        'tags': [t.strip() for t in card['Tags'].split(';')],
         'set': card['Set']
     }
 
